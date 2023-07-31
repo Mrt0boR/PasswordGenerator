6e30e2fc726882bd18bf5bca8a1595ed409efcc6
James <122300934+Mrt0boR@users.noreply.github.com>
2023-07-31 21:00:06 +0100

    Method and Class Separation Groundwork
    Gone right back to basics laying down the groundwork for having fully separate files for each method and class so that the main method is easy to approach and understand and not stacked with GUI commands alongside running the overall groundwork. Due to all the files needing to call upon variables which lie in other files. Specifically, the GUI.java  taking the generated password variable and displaying it as a JLabel, as such, you can see me learning and discerning just how I go about this.

---
+++ passwordupdate/Main.java
@@ -0,0 +1,10 @@
+package passwordupdate;
+import java.awt.Color;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Gui.gui();
+    //add the method for the password generator in here
+    }
+}
